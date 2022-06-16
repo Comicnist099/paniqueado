79e94465ca3fa22e1ed21263e1ec3caf2e17b086
Comicnist099 <marco.ctu@hotmail.com>
2022-06-16 09:21:08 -0500

    Colisiones mapa

--- paniqueados2/Game1.cs
+++ paniqueados2/Game1.cs
@@ -13,12 +13,36 @@ namespace paniqueados2
         private SpriteBatch _spriteBatch;
         private Texture2D _textura;
         private Rectangle _rectangule;
+        private Vector2 posicionPlayer;
 
-        private Vector2 posicion;
-SpriteFont font;
-        public Game1()
+//Rastroo
+         private Texture2D _texturaRastro;
+        private Rectangle _rectanguleRastro;
+        SpriteFont font;
+
+        int LimitX=1000;
+        int LimitY=700;
+
+
+public void LimitMap(){
+    if(posicionPlayer.X<=0){
+           posicionPlayer.X=0;
+    }
+      if(posicionPlayer.X>=LimitX){
+           posicionPlayer.X=LimitX-10;
+    }
+     if(posicionPlayer.Y<=0){
+           posicionPlayer.Y=0;
+    }
+     if(posicionPlayer.Y>=LimitY){
+           posicionPlayer.Y=LimitY-10;
+    }
+}
+            public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+
+
             Content.RootDirectory = "Content";
             
             IsMouseVisible = true;
@@ -26,18 +50,23 @@ SpriteFont font;
 
         protected override void Initialize()
         {
+         
             // TODO: Add your initialization logic here
-
+            _graphics.PreferredBackBufferWidth=LimitX;
+            _graphics.PreferredBackBufferHeight=LimitY;
+            _graphics.IsFullScreen=false;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            posicion=new Vector2(0,0);
+            posicionPlayer=new Vector2(0,0);
             font =Content.Load<SpriteFont>("File");
 
             _textura =Content.Load<Texture2D>("puntito");
+            _texturaRastro =Content.Load<Texture2D>("rastro");
 
             // TODO: use this.Content to load your game content here
         }
@@ -45,20 +74,22 @@ SpriteFont font;
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-                                Debug.WriteLine("holaa");
+                Exit(); 
 
-               _rectangule=new Rectangle((int)posicion.X,(int)posicion.Y,10,10);
+                LimitMap();
+
+               _rectangule=new Rectangle((int)posicionPlayer.X,(int)posicionPlayer.Y,10,10);
+               _rectanguleRastro=new Rectangle((int)posicionPlayer.X,(int)posicionPlayer.Y,10,10);
+
                 if(Keyboard.GetState().IsKeyDown(Keys.Right)){
-                posicion.X+=10;}
+                posicionPlayer.X+=10;}
                 if(Keyboard.GetState().IsKeyDown(Keys.Left)){
-                posicion.X-=10;}
+                posicionPlayer.X-=10;}
                 if(Keyboard.GetState().IsKeyDown(Keys.Up)){
-                posicion.Y-=10;}
+                posicionPlayer.Y-=10;}
                 if(Keyboard.GetState().IsKeyDown(Keys.Down)){
-                posicion.Y+=10;}
-
-
+                posicionPlayer.Y+=10;}
+               
 
 
 
@@ -80,8 +111,8 @@ SpriteFont font;
                 Vector2 textMiddlePoint = font.MeasureString("text") / 2;
                 Vector2 position2 = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2);
 
-                string playerX = new StringBuilder().Append(posicion.X).ToString();
-                string playerY = new StringBuilder().Append(posicion.Y).ToString();
+                string playerX = new StringBuilder().Append(posicionPlayer.X).ToString();
+                string playerY = new StringBuilder().Append(posicionPlayer.Y).ToString();
 
                 _spriteBatch.DrawString(font, "X:"+playerX+" Y:"+playerY, position2, Color.White, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
 
